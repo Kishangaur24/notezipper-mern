6065264ebdc565278a39a1b56ac1b3b513c88043
Kishangaur24 <114826113+Kishangaur24@users.noreply.github.com>
2023-07-04 11:58:24 +0530

    dotenv file

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,22 @@
 const express = require("express")
 const app = express()
+const notes = require("./data/const")
+const dotenv = require("dotenv")
 
+dotenv.config()
 app.get("/",(req,res)=>{
     res.send("Api is Running")
 })
 
-app.listen(5000,console.log("Server port running on 5000"))
+app.get("/api/notes",(req,res)=>{
+    res.json(notes)
+})
+app.get("/api/notes/:id",(req,res)=>{
+    const note = notes.find((n)=>n._id===req.params.id)
+    res.send(note)
+
+})
+
+const PORT = process.env.PORT || 5000
+
+app.listen(PORT,console.log(`Server port running on ${PORT}`))
