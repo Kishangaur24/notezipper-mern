102c082290efa166ae843aab3d18c2b862720e64
Kishangaur24 <114826113+Kishangaur24@users.noreply.github.com>
2023-07-04 11:01:54 +0530

    backend setup

---
+++ backend/server.js
@@ -0,0 +1,8 @@
+const express = require("express")
+const app = express()
+
+app.get("/",(req,res)=>{
+    res.send("Api is Running")
+})
+
+app.listen(5000,console.log("Server port is running on 5000"))
