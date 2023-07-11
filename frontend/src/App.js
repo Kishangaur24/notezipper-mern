a116ff2ade9e8bb20249221fe5863a90f700a7a8
Kishangaur24 <114826113+Kishangaur24@users.noreply.github.com>
2023-07-11 12:02:13 +0530

    Landing page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,13 +2,16 @@
 import './App.css';
 import Header from './components/Header/Header';
 import Footer from './components/Footer/Footer';
+import LandingPage from './screens/LandingPage/LandingPage';
 
 function App() {
   return (
    <>
       <Header/>
-      {/* <main style={{minHeight:"90vh"}}></main> */}
-      {/* <Footer/> */}
+      <main>
+        <LandingPage/>
+      </main>
+   <Footer/>
      
       </>
   );
